94252154963576bb6458de750d6fefd57173fe52
robbfreitas <robertocompromissos@gmail.com>
2022-03-29 10:46:00 -0300

    Teclas funcionando

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(idAudio);
+
+   tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador += 1;
+
+    console.log(contador);
+}

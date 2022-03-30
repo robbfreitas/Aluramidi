6297150c5e66fcfd5408f9a5e84845f3e763af28
robbfreitas <robertocompromissos@gmail.com>
2022-03-30 09:00:17 -0300

    While substituido por For

--- main.js
+++ main.js
@@ -6,20 +6,14 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
-
-    console.log(idAudio);
 
    tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador += 1;
-
-    console.log(contador);
 }
